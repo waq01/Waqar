87f29e38c8746bb1c6254b4379a0311f056685e7
waq01 <waqaradmin@gmail.com>
2026-04-10 22:13:04 +0300

    feat: implement SplashScreen API to eliminate cold start flash

--- app/src/main/java/com/xstudio/waqar/MainActivity.kt
+++ app/src/main/java/com/xstudio/waqar/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.xstudio.waqar
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.*
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.xstudio.waqar.ui.screens.SplashScreen
 import com.xstudio.waqar.ui.theme.WaqarTheme
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
